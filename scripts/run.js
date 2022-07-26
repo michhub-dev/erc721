f1fbd9d2ff9dd87f2b88b6d02976d62ebf792f24
Michelle David <rachaelholy@gmail.com>
2022-07-26 01:18:04 +0100

    Updated script

--- scripts/run.js
+++ scripts/run.js
@@ -4,3 +4,14 @@ const main = async () => {
     await contractFactory.deployed();
     console.log("Contract deployed to..", contractFactory.address) 
 }
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0)
+    } catch(error) {
+       console.log("Couldn't deploy", error);
+       process.exit(1);
+    }
+
+}
+runMain(); 
