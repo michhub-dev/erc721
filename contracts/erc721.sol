068a2a7b14a92ce5a347a33eb8c11c63dbe8bedf
Michelle David <rachaelholy@gmail.com>
2022-07-28 13:17:24 +0100

    Mint function

--- contracts/erc721.sol
+++ contracts/erc721.sol
@@ -1,9 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9; 
+pragma solidity ^0.8.1; 
+// Import OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ERC721Token {
-    
-    constructor() erc721("Oiler","O") {
-        console.log("ERC721")
+//Inherit the imported contract
+contract ERC721Token is ERC721URIStorage {
+ // this helps to keep track of tokenId
+    using Counters for Counters.Counter; 
+    Counters.Counter private _tokenIds;
+
+// pass the token name and symbol 
+    constructor() ERC721("Oiler","O") {
+        console.log("ERC721");
+    }
+    // Function to mint nft
+    function mintNft () public {
+        // get current tokenId 
+        uint newTokenId = _tokenIds.current();
+        //mint nft to the sender
+        _SafeMint(msg.sender, newTokenId);
+
+        //nft data
+        _setTokenUrI(newTokenId, blabla);
+        
+        //Increment tokenId for the next nft that will be minted
+        _tokenIds.Increment();
     }
 }
