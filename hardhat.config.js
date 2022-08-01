c22e0d6eee934b0515b323a9117dfb47fdc951a8
Michelle David <rachaelholy@gmail.com>
2022-08-01 18:17:31 +0100

    Installed .env, changed config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.9",
-};
+//require('@nomiclabs/hardhat-waffle');
+require("dotenv").config({ path: ".env" });
 
+module.exports = {
+  solidity: '0.8.9',
+  networks: {
+    goerli: {
+      url: process.env.QUICKNODE_API_KEY_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    },
+  },
+};
